1b799bbac80faadf2c4ee7f697ca5144bdf856c4
ujued <ujued@qq.com>
2018-05-16 23:20:38 +0800

    优化连接自动关闭，连接的活跃保持，初步构想

--- spmpool.py
+++ spmpool.py
@@ -1,15 +1,10 @@
-from queue import Queue
-from threading import Thread
+from queue import Queue, Empty
 from pymysql import connections
 from datetime import datetime
-from time import sleep
+from threading import Thread
+
+import time
 import logging
-
-
-# 日志配置
-logging.basicConfig(level=logging.INFO, filename='spmpool.log', filemode='w',
-        format='%(asctime)s %(pathname)s %(filename)s[line:%(lineno)d] %(levelname)s %(message)s ',
-        datefmt='%a, %d %b %Y %H:%M:%S')
 
 
 class Connection(object):
@@ -19,7 +14,7 @@ class Connection(object):
         self._pool = pool
         self.result = None
         self.proto = connections.Connection(**kwargs)
-        self._restore()
+        self.restore()
 
     def __enter__(self):
         return self
@@ -27,11 +22,10 @@ class Connection(object):
     def __exit__(self, exc_type, exc_val, exc_tb):
         self.close()
 
-    def _restore(self):
+    def restore(self):
         self._closed = False
         self.proto.autocommit(True)
         self.result = None
-        logging.info('restore a connection.')
 
     def _check(self):
         if self._closed:
@@ -56,7 +50,7 @@ class Connection(object):
         self._check()
         cursor = self.proto.cursor()
         cursor.execute(sql)
-        self.result = ResultProxy(cursor)
+        self.result = ResultProxy(cursor, self._pool)
         return self.result
 
     def close(self):
@@ -71,8 +65,8 @@ class Connection(object):
 
 class ConnectionPool(object):
     """ 连接池实现 """
-    _configs = {
-        'dev':{
+    configs = {
+        'dev': {
             'user': 'root',
             'password': '123456',
             'database': 'test',
@@ -82,49 +76,76 @@ class ConnectionPool(object):
             'init_size': 10
         }
     }
-    _instances = {}  # 所有数据源
-    _resources = []  # 可以被关闭的资源，connection，cursor
-    _res_queue = Queue(20000 * 2)  # 待关闭队列，两种资源，所以是20000的2倍
+    instances = {}  # 所有数据源
 
     def __init__(self, init_size, **kwargs):
         self._init_size = init_size
-        self._queue = Queue(20000) # the queue of connection wrapper
-        self.stopped = False
+        self._get_count = 0
+        self._queue = Queue(20000)  # the queue of connection wrapper
+        self.resources = []
+        self._ready_closed_resources = Queue(20000 * 2)
+        self._stopped = False
         self.kwargs = kwargs
+        self.name = self.__class__.__name__
         self.startup()
+
+    def __keep_alive_runner(self):
+        pass
+
+    def __monitor_runner(self):
+        """ 每隔300秒检测一次未手动关闭的资源，将其加入待关闭队列 """
+        while True:
+            logging.info('%s has %d resources need to check to close.' % (self.name, len(self.resources)))
+            for r in self.resources:
+                if (datetime.now() - r['put_time']).seconds > 150:
+                    self.resources.remove(r)
+                    self._ready_closed_resources.put(r['value'])
+            time.sleep(300)
+
+    def __cleaner_runner(self):
+        """ 执行关闭 """
+        while True:
+            resource = self._ready_closed_resources.get()
+            if resource:
+                resource.close()
+                logging.info('a connection auto closed.')
 
     def put(self, conn):
         self._queue.put(conn)
 
     def get(self):
-        if self.stopped:
+        if self._stopped:
             logging.exception("this connection pool is stopped. run this connection pool's startup() is useful!")
             return
         try:
             conn = self._queue.get_nowait()
+            self._get_count += 1
             logging.info('get a connection.')
-        except Exception as e:
-            print('the init_size of ConnectionPool is so small ! waiting...')
+        except Empty:
+            print('The init_size of ConnectionPool is empty ! waiting...')
             conn = self._queue.get()
-        ConnectionPool._resources.append({'pdate': datetime.now(), 'value': conn})
-        conn._restore()
+        self.resources.append({'put_time': datetime.now(), 'value': conn})
+        conn.restore()
         return conn
 
     def size(self):
         return self._queue.qsize()
 
-    def useable(self):
-        return True if self._queue.qsize() > 0 else False
-
-    def extend(self, poolorsize=10):
-        if type(poolorsize) == ConnectionPool:
-            while poolorsize.readycount() > 0:
-                self._queue.put(poolorsize.get())
-        elif type(poolorsize) == int:
-            for i in range(poolorsize):
+    def extend(self, pool_or_size=10):
+        """ 扩展连接池 """
+        if type(pool_or_size) == ConnectionPool:
+            assert isinstance(pool_or_size, ConnectionPool)
+            while pool_or_size.size() > 0:
+                self._queue.put(pool_or_size.get())
+        elif type(pool_or_size) == int:
+            for i in range(pool_or_size):
                 self._queue.put(Connection(self, **self.kwargs))
         else:
             return
+
+    def usable(self):
+        """ 连接池是否可用 """
+        return True if self._queue.qsize() > 0 else False
 
     def startup(self):
         try:
@@ -133,21 +154,34 @@ class ConnectionPool(object):
         except Exception as e:
             logging.exception('Warning:%s' % e)
             logging.exception('%d connections create successful !' % self._queue.qsize())
-        self.stopped = False
+        self._stopped = False
 
     def shutdown(self):
         while self._queue.qsize() > 0:
             self._queue.get().shutdown()
-        self.stopped = True
+        self._stopped = True
 
     def gc(self):
         while self._queue.qsize() > 0:
             self._queue.get().close()
 
+    def keep_alive(self):
+        kal_t = Thread(target=self.__keep_alive_runner)  # 一定的策略保持connection一直存活
+        monitor_t = Thread(target=self.__monitor_runner)
+        cleaner_t = Thread(target=self.__cleaner_runner)
+        kal_t.setDaemon(True)
+        monitor_t.setDaemon(True)
+        cleaner_t.setDaemon(True)
+        kal_t.start()
+        monitor_t.start()
+        cleaner_t.start()
+
 
 class ResultProxy(object):
     """ 查询结果代理对象 """
-    def __init__(self, cursor):
+    def __init__(self, cursor, pool):
+        self._pool = pool
+        self._all_results = None
         self.cursor = cursor
         self.rowcount = cursor.rowcount
 
@@ -155,21 +189,24 @@ class ResultProxy(object):
         data = self.cursor.fetchone()
         row = None
         if data:
-            row = self._2row(data, self.cursor.description)
+            row = ResultProxy._2row(data, self.cursor.description)
             self.cursor.scroll(-1)
-        ConnectionPool._resources.append({'pdate':datetime.now(), 'value': self.cursor})
+        self._pool.resources.append({'put_time': datetime.now(), 'value': self.cursor})
         return row
 
     def fetchall(self):
         data = self.cursor.fetchall()
         result = []
         desc = self.cursor.description
-        for item in data : result.append(self._2row(item, desc))
-        if self.rowcount > 0 : self.cursor.scroll(-self.rowcount)
-        ConnectionPool._resources.append({'pdate': datetime.now(), 'value': self.cursor})
+        for item in data:
+            result.append(ResultProxy._2row(item, desc))
+        if self.rowcount > 0:
+            self.cursor.scroll(-self.rowcount)
+        self._pool.resources.append({'put_time': datetime.now(), 'value': self.cursor})
         return result
 
-    def _2row(self, proto_row, desc):
+    @classmethod
+    def _2row(cls, proto_row, desc):
         row = _Row()
         for i in range(len(desc)):
             col_name = desc[i][0]
@@ -181,16 +218,17 @@ class ResultProxy(object):
 
     def __iter__(self):
         result = self.fetchall()
-        self.allresult = result
+        self._all_results = result
         return result.__iter__()
 
     def __next__(self):
-        self.allresult.__next__()
+        self._all_results.__next__()
 
 
 class _Row(object):
     """ 数据表行对象 """
-    def __init__(self) : self.index = []
+    def __init__(self):
+        self.index = []
 
     def __getitem__(self, key):
         if type(key) == int:
@@ -209,15 +247,15 @@ def add_config(
         charset='utf8',
         init_pool_size=10):
     """ 添加数据源配置 """
-    ConnectionPool._configs.update({
-            pool_name:{
-                'user':user,
-                'password':password,
-                'database':database,
-                'host':host,
-                'port':port,
-                'charset':charset,
-                'init_size':init_pool_size
+    ConnectionPool.configs.update({
+            pool_name: {
+                'user': user,
+                'password': password,
+                'database': database,
+                'host': host,
+                'port': port,
+                'charset': charset,
+                'init_size': init_pool_size
             }
         }
     )
@@ -225,15 +263,15 @@ def add_config(
 
 def spmpool(pool_name='dev'):
     """ 获取某个数据源的连接池 """
-    if pool_name not in ConnectionPool._instances.keys():
-        if pool_name not in ConnectionPool._configs.keys():
+    if pool_name not in ConnectionPool.instances.keys():
+        if pool_name not in ConnectionPool.configs.keys():
             print('none this config.')
             return
         else:
-            config = ConnectionPool._configs[pool_name]
-            ConnectionPool._instances.update(
+            config = ConnectionPool.configs[pool_name]
+            ConnectionPool.instances.update(
                 {
-                    pool_name:ConnectionPool(
+                    pool_name: ConnectionPool(
                         config['init_size'],
                         host=config['host'],
                         user=config['user'],
@@ -243,46 +281,14 @@ def spmpool(pool_name='dev'):
                         charset=config['charset']
                     )
                 }
-            )           
-    return ConnectionPool._instances[pool_name]
-
-
-def _resource_monitor(condition):
-    """ 每隔300秒检测一次未手动关闭的资源，将其加入待关闭队列 """
-    while condition:
-        logging.info('%d resources need to close.' % len(ConnectionPool._resources))
-        for r in ConnectionPool._resources:
-            if (datetime.now() - r['pdate']).seconds > 150:
-                ConnectionPool._resources.remove(r)
-                ConnectionPool._res_queue.put(r['value'])
-        sleep(300)
-    logging.info('Monitor stopped.')
-
-
-def _resource_cleaner(condition):
-    """ 执行关闭 """
-    while condition:
-        r = ConnectionPool._res_queue.get()
-        if r:
-            r.close()
-            logging.info('a connection auto closed.')
-    logging.info('Cleaner stopped.')
-
-
-def _spmpool_startup():
-    """ 启动连接池 """
-    monitor = Thread(target=_resource_monitor, args=(True,))
-    cleaner = Thread(target=_resource_cleaner, args=(True,))
-    monitor.setDaemon(True)
-    cleaner.setDaemon(True)
-    monitor.start()
-    cleaner.start()
+            )
+    pool = ConnectionPool.instances[pool_name]
+    assert isinstance(pool, ConnectionPool)
+    pool.keep_alive()
+    return pool
 
 
 def shutdown():
     """ 关闭连接池 """
-    for instance in ConnectionPool._instances.values():
+    for instance in ConnectionPool.instances.values():
         instance.shutdown()
-
-
-_spmpool_startup()
